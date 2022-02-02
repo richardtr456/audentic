0b09bcf52737645f831891d96c64fc65d1e83831
richardtr456 <richard.t1@hotmail.com>
2022-02-02 16:21:45 -0500

    go con bootstrap

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+        <link rel="stylesheet" href="<?= base_url()?>/public/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
 <form action="autenticar" method="post">
         <label for="usuario">Usuario:</label>
         <input type="text" name="usuario" id="usuario"><br>
@@ -5,5 +15,9 @@
         <input type="password" name="contrasena" id="contrasena"><br>
         <input type="submit" name="submit" value="Iniciar sesión">
 </form>
+  
+</body>
+</html>
+
 
 
